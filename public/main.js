917152dbea113720a8f28f29077f5aadfffe6cf6
ABDOUL BAGUI <abdoulbagui094@gmail.com>
2023-12-30 16:31:10 +0100

    Première version de mon CV

---
+++ public/main.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('load', function () {
+    document.getElementById("load").style.display = "none";
+    document.getElementsByTagName('main').item(0).style.display = "block";
+    document.getElementsByTagName('nav').item(0).style.display = "block";
+});
